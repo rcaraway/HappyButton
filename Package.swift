64e8cf600e0624183cad5e0f404685a7addc9fa9
Rob Caraway <robbor911@gmail.com>
2020-04-11 01:09:53 -0500

    button commit done

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "HappyButton",
+    platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -12,6 +13,7 @@ let package = Package(
             targets: ["HappyButton"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/rcaraway/HappyColors", .branch("master"))
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
@@ -20,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "HappyButton",
-            dependencies: []),
+            dependencies: ["HappyColors"]),
         .testTarget(
             name: "HappyButtonTests",
             dependencies: ["HappyButton"]),
--- Sources/HappyButton/HappyButton.swift
+++ Sources/HappyButton/HappyButton.swift
@@ -1,3 +1,66 @@
-struct HappyButton {
-    var text = "Hello, World!"
+import HappyColors
+import UIKit
+
+public enum HappyButtonType: String {
+    case confirm
+    case cancel
+    case normal
+    case normalTransluscent
 }
+
+var hapticButtons = [String: String]()
+
+public extension UIButton {
+    
+    func setStyling(_ style: HappyButtonType) {
+        titleLabel?.font = .systemFont(ofSize: 19, weight: .medium)
+        setTitleColor(.flatWhite, for: .normal)
+        setTitleColor(.flatWhiteDark, for: .highlighted)
+        switch style {
+        case .normal:
+            backgroundColor = .flatBlue
+        case .cancel:
+            backgroundColor = .reject
+        case .confirm:
+            backgroundColor = .confirm
+        case .normalTransluscent:
+            backgroundColor = .clear
+            layer.borderColor = UIColor.flatBlue.cgColor
+            layer.borderWidth = 2.0
+            setTitleColor(.flatBlue, for: .normal)
+            setTitleColor(.flatBlueDark, for: .highlighted)
+        }
+        
+        layer.cornerRadius = 12
+        let key = "\(Unmanaged.passUnretained(self).toOpaque())"
+        hapticButtons[key] = style.rawValue
+        addTarget(self, action: #selector(hb_haptic_tapped), for: .touchUpInside)
+    }
+    
+    func setStyling(_ style: HappyButtonType,
+                    text: String) {
+        setTitle(text, for: .normal)
+        setStyling(style)
+    }
+    
+    @objc private func hb_haptic_tapped() {
+        let key = "\(Unmanaged.passUnretained(self).toOpaque())"
+        guard let style = hapticButtons[key] else {
+            return
+        }
+        if style == "confirm" {
+        
+            let generator = UINotificationFeedbackGenerator()
+                       generator.notificationOccurred(.success)
+
+        } else if style == "cancel"  {
+                      let generator = UIImpactFeedbackGenerator(style: .light)
+            generator.impactOccurred()
+
+        } else {
+            let generator = UIImpactFeedbackGenerator(style: .medium)
+            generator.impactOccurred()
+        }
+    }
+}
+
